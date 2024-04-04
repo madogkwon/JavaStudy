a3b051ce27d331cdba64befcda37c8f33ef3b555
madogkwon <a93081077a@gmail.com>
2024-04-04 23:53:01 +0900

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+  int num1;
 
-        System.out.println("Hello World");
-    }
+  static int num2;
+
+  public void printName(String name) {
+    String prtMsg = name + " Hello";
+    System.out.println(prtMsg);
+  }
+  public static void main(String[] args) {
+    Main mc = new Main();
+    mc.num1 = 10;
+    Main.num2 = 20;
+
+    mc.printName("홍길동");
+
+    System.out.printf("%d\n%d", mc.num1, num2);
+  }
 }
