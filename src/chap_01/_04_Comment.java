a419953b8aee51f0b44637106c41a70778821bb9
madogkwon <a93081077a@gmail.com>
2024-04-11 09:27:08 +0900

    Changes

---
+++ src/chap_01/_04_Comment.java
@@ -0,0 +1,19 @@
+package chap_01;
+
+public class _04_Comment {
+  public static void main(String[] args) {
+    System.out.println("(10분 전) 잠시 후 시험이 시작됩니다.");
+    //System.out.println("(5분 전) 잠시 후 시험이 시작됩니다.");
+    System.out.println("지금부터 시험을 시작합니다.");
+
+    int size = 120;
+    size = size + 10; //사이즈 10만큼 추가
+    System.out.println("신발 사이즈 " + size + "주세요");
+
+    // 한 줄 주석 단축키 : Ctrl + /
+    // 여러 줄 주석 단축키 : Ctrl + Shift + /
+    int a= 10;
+    int b = 20;
+    System.out.println(a+b); // 30
+  }
+}
