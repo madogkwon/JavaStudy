b39c258719e99b34802a09613ffae4dcea2a8bd2
madogkwon <a93081077a@gmail.com>
2024-04-15 19:28:57 +0900

    Changes

---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,19 @@
+package chap_01;
+
+/*
+(실행 결과)
+상암08번 버스
+약 3분 후 도착
+남은 거리 1.2km
+ */
+
+public class _Quiz_01 {
+  public static void main(String[] args) {
+    String BusNo = "상암08";
+    int time = 3;
+    double distance = 1.2;
+    System.out.println(BusNo + "번 버스");
+    System.out.println("약 " + time + "분 후 도착");
+    System.out.println("남은 거리 " + distance + "km");
+  }
+}
